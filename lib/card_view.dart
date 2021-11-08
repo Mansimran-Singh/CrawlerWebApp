f290ed4eedb7f79e12da6105e5c340fb1aee068f
Mansimran Singh <mansimran.singh@codecorp.com>
2021-11-08 18:37:07 -0500

    clean up

--- lib/card_view.dart
+++ lib/card_view.dart
@@ -19,74 +19,137 @@ class CardView extends StatelessWidget {
       color: Colors.white38,
       elevation: 5,
       child: CachedNetworkImage(
-          imageUrl: _posterUrl,
-          imageBuilder: (context, imageProvider) => Container(
-                decoration: BoxDecoration(
-                  image:
-                      DecorationImage(image: imageProvider, fit: BoxFit.fill),
+        imageUrl: _posterUrl,
+        imageBuilder: (context, imageProvider) => Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(image: imageProvider, fit: BoxFit.fill),
+          ),
+          child: 
+          Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Card(
+                shape: const RoundedRectangleBorder(
+                  borderRadius:
+                      BorderRadius.only(bottomRight: Radius.circular(40)),
                 ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Card(
-                      shape: const RoundedRectangleBorder(
-                        borderRadius:
-                            BorderRadius.only(bottomRight: Radius.circular(40)),
-                      ),
-                      color: Colors.white70,
-                      child: Padding(
-                        padding: const EdgeInsets.all(18.0),
-                        child: Text(
-                          _releaseData,
-                          textAlign: TextAlign.left,
-                          style: const TextStyle(
-                            color: Colors.black87,
-                            fontSize: 12.0,
-                            fontWeight: FontWeight.bold
-                          ),
-                        ),
-                      ),
-                    ),
-                    Card(
-                      shape: const RoundedRectangleBorder(
-                        borderRadius: BorderRadius.only(
-                            topLeft: Radius.circular(40),
-                            topRight: Radius.circular(40)),
-                      ),
-                      color: Colors.black87,
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Center(
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: [
-                              Text(
-                                _title,
-                                style: const TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                    color: Colors.white,
-                                    fontSize: 16),
-                                textAlign: TextAlign.center,
-                              ),
-                              Text(
-                                _url,
-                                style: const TextStyle(
-                                    fontWeight: FontWeight.normal,
-                                    color: Colors.white,
-                                    fontSize: 10),
-                                textAlign: TextAlign.center,
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ),
-                  ],
+                color: Colors.white70.withOpacity(0.5),
+                child: Padding(
+                  padding: const EdgeInsets.all(18.0),
+                  child: Text(
+                    _releaseData,
+                    textAlign: TextAlign.left,
+                    style: const TextStyle(
+                        color: Colors.black87,
+                        fontSize: 12.0,
+                        fontWeight: FontWeight.bold),
+                  ),
                 ),
               ),
-          placeholder: (context, url) => const CircularProgressIndicator(),
-          errorWidget: (context, url, error) => const Icon(Icons.error)),
+              Card(
+                shape: const RoundedRectangleBorder(
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(40),
+                      topRight: Radius.circular(40)),
+                ),
+                color: Colors.black87.withAlpha(20),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Center(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text(
+                          _title,
+                          style: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                              fontSize: 16),
+                          textAlign: TextAlign.center,
+                        ),
+                        Text(
+                          _url,
+                          style: const TextStyle(
+                              fontWeight: FontWeight.normal,
+                              color: Colors.white,
+                              fontSize: 10),
+                          textAlign: TextAlign.center,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        placeholder: (context, url) => const Center(
+          child: SizedBox(
+                  child: CircularProgressIndicator(),
+                  height: 10.0,
+                  width: 10.0,
+                ),
+        ),
+        errorWidget: (context, url, error) => Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Card(
+              shape: const RoundedRectangleBorder(
+                borderRadius:
+                    BorderRadius.only(bottomRight: Radius.circular(40)),
+              ),
+              color: Colors.white70.withOpacity(0.5),
+              child: Padding(
+                padding: const EdgeInsets.all(18.0),
+                child: Text(
+                  _releaseData,
+                  textAlign: TextAlign.left,
+                  style: const TextStyle(
+                      color: Colors.black87,
+                      fontSize: 12.0,
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
+            ),
+            Card(
+              shape: const RoundedRectangleBorder(
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(40),
+                    topRight: Radius.circular(40)),
+              ),
+              color: Colors.black87.withAlpha(20),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Center(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text(
+                        _title,
+                        style: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                            fontSize: 16),
+                        textAlign: TextAlign.center,
+                      ),
+                      Text(
+                        _url,
+                        style: const TextStyle(
+                            fontWeight: FontWeight.normal,
+                            color: Colors.white,
+                            fontSize: 10),
+                        textAlign: TextAlign.center,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
